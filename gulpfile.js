c579769a12f8581fec512b0719c324ea7ab823df
Michael Akbaraly <makbaraly@octo.com>
2015-05-10 12:12:49 +0200

    - configure gulp and karma

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,32 @@
-var gulp = require('gulp');
+var gulp = require("gulp");
+var connect = require("gulp-connect");
+var karma = require("gulp-karma");
 
-gulp.task('default', function() {});
+gulp.task("connect", function () {
+  connect.server({
+    root: "app",
+    livereload: true
+  });
+});
+
+gulp.task("html", function () {
+  gulp.src("./app/*.html")
+    .pipe(connect.reload());
+});
+
+gulp.task("watch", function () {
+  gulp.watch(["./app/*.html"], ["html"]);
+});
+
+gulp.task("test", function () {
+  return gulp.src("test/*")
+    .pipe(karma({
+      configFile: "karma.conf.js",
+      action: "run"
+    }))
+    .on("error", function (err) {
+      throw err;
+    });
+});
+
+gulp.task("default", ["connect", "watch"]);
